f6edf6b13c3e50d164733b66eb0ea52dd07dcc55
liux15 <2425264566@qq.com>
2018-05-04 16:22:30 +0800

    the first time add files to git using gitBase

---
+++ GetSetTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using Mysoft.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestModel test = new TestModel();
+            DateTime start = DateTime.Parse("2017-01");
+            DateTime end = start.AddMonths(1).AddDays(-1);
+            Console.WriteLine(start.ToString("yyyy-MM-dd") + " | " + end.ToString("yyyy-MM-dd"));
+
+            List<string> ts = new List<string>()
+            {
+                "","","北京","深圳"
+            };
+
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            foreach(var temp in ts){
+                dic.Add(temp,temp);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ GetSetTest/TestModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetTest
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+
+
+        public TestModel()
+        {
+            Name = "Defalut Name";
+        }
+
+
+        public TestModel(string name)
+        {
+            Name = name;
+        }
+
+
+
+        public string getName(){
+            return Name;
+        }
+    }
+}
